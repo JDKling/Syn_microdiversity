8c812a1697c7ad9ca1ba0c086a273246d74c73ee
Joshua Kling <joshuakling13@gmail.com>
2021-06-28 09:28:16 -0700

    Add files via upload

--- SCRIPTS/DL_data.sh
+++ SCRIPTS/DL_data.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 mkdir -p DBs
+mkdir -p LOGS
+
+# Setup log files
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>LOGS/DL_data_log.out 2>&1
 
 # download sequence reads
 ## create and environment that will let us download the raw seq data we are interested in
